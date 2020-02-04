102122ead776bea00e6e23be927ae6ac7417a475
youchan <youchan01@gmail.com>
2020-02-04 10:14:39 +0900

    Make enable font-awesome

--- webpack/production.js
+++ webpack/production.js
@@ -58,7 +58,16 @@ const common_config = {
             },
             {
                 test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
-                use: [ "cache-loader", "file-loader" ]
+                use: [
+                  { loader: "cache-loader" },
+                  {
+                    loader: "file-loader",
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'fonts/'
+                    }
+                  }
+                ]
             },
             {
                 // opal-webpack-loader will compile and include ruby files in the pack
